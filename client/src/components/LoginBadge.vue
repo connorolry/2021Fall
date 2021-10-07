b993eda0e036b3ce9576e0aaf6a385c123fdfdad
connorolry <61296104+connorolry@users.noreply.github.com>
2021-10-07 14:00:35 -0400

    vue loginbadge

---
+++ client/src/components/LoginBadge.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="buttons" v-if="!name">
+      <a class="button is-primary">
+       <strong>Sign up</strong>
+     </a>
+       <a class="button is-light" @click="login">
+            Log in
+        </a>
+    </div> 
+    <div v-else>
+                      Hello{{name}}
+    </div>
+</template>
+
+<script>
+import Session from "../services/session"
+export default {
+    data: ()=>({ 
+        name: null,
+        Session
+    }),
+    method: {
+        login() {
+            this.name = 'Bob';
+        }
+    },
+    computed:{
+        name(){
+            return this.Session.user.FirstName + ' ' + this.Session.user.Lastname
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
